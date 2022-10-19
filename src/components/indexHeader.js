598eb71f0fac5409931d51f3d06ea4287970948d
ViniciusCalo <vini.stcal@gmail.com>
2022-10-19 08:08:50 -0300

    building the header

--- src/components/indexHeader.js
+++ src/components/indexHeader.js
@@ -1,28 +1,55 @@
-import {AiOutlineSearch, AiFillHome} from 'react-icons/ai'
-import {RiMessengerLine} from 'react-icons/ri'
-import {BsPlusSquare} from 'react-icons/bs'
-import {MdOutlineExplore} from 'react-icons/md'
-import {FiHeart} from 'react-icons/fi'
-import "./styleHeader.css"
+import { AiOutlineSearch, AiFillHome } from 'react-icons/ai'
+import { RiMessengerLine } from 'react-icons/ri'
+import { BsPlusSquare } from 'react-icons/bs'
+import { MdOutlineExplore } from 'react-icons/md'
+import { FiHeart } from 'react-icons/fi'
+
+import { IconContext } from 'react-icons'
+
+import './styleHeader.css'
 
 export function Header() {
+
     return (
-        <header className="header">
-            <div className="container">
-                <img className="logo" src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" />
+        <header className="header" >
+
+            <div className="container" >
+
+                <img className="logo" src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" alt='logo' />
+
+                <div className="inputFake">
+                   <IconContext.Provider value={{ color: '#8e8e8e' }}>
+                        <AiOutlineSearch />
+                    </IconContext.Provider>
+
+                    <input placeholder="pesquisar" />
+                </div>
+
+                <div className="menu-icons" >
+                    <IconContext.Provider value={{ size: '26px' }}>
+                        <div>
+                            <AiFillHome />
+                        </div>
+                        <div>
+                            <RiMessengerLine />
+                        </div>
+                        <div>
+                            <BsPlusSquare />
+                        </div>
+                        <div>
+                            <MdOutlineExplore />
+                        </div>
+                        <div>
+                            <FiHeart />
+                        </div>
+                    </IconContext.Provider>
+
+
+                </div>
+
             </div>
 
-            <div className="input-fake">
-                <AiOutlineSearch />
-                <input />
-            </div>
-            <div>
-               <AiFillHome />
-               <RiMessengerLine />
-               <BsPlusSquare />
-               <MdOutlineExplore />
-               <FiHeart /> 
-            </div>
-        </header>
+        </header >
     );
+
 }
